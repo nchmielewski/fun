5cb52c13e8523bb956229e8532c0efdf332eef1b
Nicholas Chmielewski <nicholas.chmielewski.j@gmail.com>
2023-04-05 23:31:25 -0700

    done

--- rock-paper-scissor/main.py
+++ rock-paper-scissor/main.py
@@ -14,13 +14,25 @@ def convert(s):
 def verdict(i, com):
     if i == com:
         return 2
+    
+    match i:
+        case 'Rock':
+            if com == 'Scissors': return 0
+            else: return 1
 
+        case 'Paper':
+            if com == 'Rock': return 0
+            else: return 1
+
+        case 'Scissors':
+            if com == 'Paper': return 0
+            else: return 1
 
 #driver
-score = '0 - 0 - 0'
+score = [0, 0, 0]
+print("Let's play rock paper scissors.  The computer will randomly pick an option.", '\n')
 while True:
-    print("Let's play rock paper scissors.  The computer will randomly pick an option.", '\n', "Enter your choice: ")
-    i = input()
+    i = input("Enter your choice: ")
 
     com = random.choice(['Rock', 'Paper' ,'Scissors'])
     i = convert(i)
@@ -33,14 +45,20 @@ while True:
 
     match v:
         case 0:
-            print(f'{i} beats {v} - Player wins!')
+            print(f'{i} beats {com} - Player wins!')
+            score[0] += 1
             
         case 1:
-            print(f'{v} beats {i} - Computer wins!')
+            print(f'{com} beats {i} - Computer wins!')
+            score[2] += 1
             
         case 2:
             print(f'Both picked {i} - Tie!')
-            
+            score[1] += 1    
+    
+    print(f'Player won {score[0]}   Computer won {score[2]}   {score[1]} Ties')
+    print('\n')  
+              
 
 
 
