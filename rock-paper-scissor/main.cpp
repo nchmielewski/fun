5af752d26cd1e5d982ec8277e6b1470fbfd47715
nick c <92892910+nchmielewski@users.noreply.github.com>
2023-04-11 21:40:08 -0700

    Update main.cpp

--- rock-paper-scissor/main.cpp
+++ rock-paper-scissor/main.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
+#include <string>
 using namespace std;
 
-
-
 int main() {
-
+  string x; 
+  cout << "Let's play rock paper scissors.  The computer will randomly pick an option." << "\n";
+  
+  bool running = true;
+  while (running) {
+    cout << "Enter your choice: " << x;
+    cin >> x; // Get user input from the keyboard
+    
+  }
+  
 }
