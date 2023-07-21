d37fb5263a1844a904cd3635f1c3105da12f7435
Nicholas Chmielewski <nicholas.chmielewski.j@gmail.com>
2023-07-20 23:10:17 -0700

    beeg

--- rock-paper-scissor/main.cpp
+++ rock-paper-scissor/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+
 using namespace std;
 
 string convert(string x){
@@ -19,17 +20,25 @@ string convert(string x){
   else return "e";
 }
 
+// return 0/nothing 1/player 2/pc
 int decide(int AI, string x) {
-  string player = convert(x);
-  
+
   switch(AI):
     case 0:
       cout << "Computer picked rock" << endl;
-      if (x == "p")
+      if (x == "p") return 1;
+      elif (x == "s") return 2;
+      else return 0;
     case 1:
       cout << "Computer picked paper" << endl;
+      if (x == "s") return 1;
+      elif (x == "r") return 2;
+      else return 0;
     case 2:
       cout << "Computer picked scissors" << endl;
+      if (x == "r") return 1;
+      elif (x == "p") return 2;
+      else return 0;
 }
 
 int main() {
@@ -41,9 +50,18 @@ int main() {
     cout << "Enter your choice: " << x << endl;
     cin >> x; // Get user input from the keyboard
     
+    string player;
+    if (convert(x) == "e") {
+      cout << "Please type normally" << endl;
+      continue;
+    }
+    elif (convert(x) == "normally") {
+      cout << "Alright wise guy, haha!" << endl;
+    }
+
     int AI = rand() % 3;    // 0 rock, 1 paper, 2 scissors
     
-    bool result = decide(AI, x);
+    bool result = decide(AI, player);
 
     if result{ return 0 }
     
